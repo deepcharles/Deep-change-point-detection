e0211e20ed3c488177e43a1b174a19bb2e580c25
Charles T <charles@doffy.net>
2024-03-29 14:32:27 +0100

    correct loss

--- src/deepcpd/loss.py
+++ src/deepcpd/loss.py
@@ -10,10 +10,12 @@ def get_mse_loss(signal: jnp.ndarray, centroids: jnp.ndarray, penalty: float) ->
     # signal, shape (n_samples, n_dims)
     # centroids, shape (n_states, n_dims)
     # penalty, float
+    # The output is the penalized mse. The penalty is n_changes * penalty.
     optimal_state_sequence = stop_gradient(
         get_switching_model(signal=signal, centroids=centroids, penalty=penalty)
     )
     approx = centroids[optimal_state_sequence]
     n_samples, n_dims = signal.shape
-    mse = jnp.square(signal - approx).sum() / n_samples
+    n_changes = jnp.count_nonzero(jnp.diff(optimal_state_sequence))
+    mse = (jnp.square(signal - approx).sum() + penalty * n_changes) / n_samples
     return mse
